3804fdb237809a66a252d8457e503dc079a4c793
Tasuku Suzuki <tasuku.suzuki@signal-slot.co.jp>
2023-12-21 17:00:17 +0900

    copy and replace upstream mcu-board-support to local

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@
 #![no_main]
 slint::include_modules!();
 
-#[mcu_board_support::entry]
+mod pico_st7789;
+pub use pico_st7789::*;
+
+#[pico_st7789::entry]
 fn main() -> ! {
-    mcu_board_support::init();
+    pico_st7789::init();
 
     let app = App::new().unwrap();
     app.global::<Keyboard>().on_send(move |shortcut| {});
